2cad4e6b9ca84ff502a5cedbfdeeff436a36701d
mojilinux <mojilinux@gmail.com>
2022-08-25 16:31:06 +0430

    initialize Spring boot app

---
+++ src/test/java/io/github/mojilinux/nabApp/NabAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mojilinux.nabApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
