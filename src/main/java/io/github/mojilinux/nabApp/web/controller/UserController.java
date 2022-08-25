7defe4a0ab61971ec99c879831c9e692f702a457
mojilinux <mojilinux@gmail.com>
2022-08-25 17:33:14 +0430

    Add UserController to project

---
+++ src/main/java/io/github/mojilinux/nabApp/web/controller/UserController.java
@@ -0,0 +1,38 @@
+package io.github.mojilinux.nabApp.web.controller;
+
+import io.github.mojilinux.nabApp.model.User;
+import io.github.mojilinux.nabApp.repository.IUserRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/users", produces = "application/json")
+public class UserController {
+
+    private final IUserRepository userRepository;
+
+    public UserController(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping()
+    public Iterable<User> getUser(int pageNumber, int pageSize){
+        PageRequest page = PageRequest.of(pageNumber, pageSize, Sort.by("createdDate").descending());
+        return userRepository.findAll(page).getContent();
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addUser(@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) throws EmptyResultDataAccessException {
+        userRepository.deleteById(id);
+    }
+}
