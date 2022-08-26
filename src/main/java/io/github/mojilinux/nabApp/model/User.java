382c856bb05595aaf82bd69c1b78a56a06e1fc32
mojilinux <mojilinux@gmail.com>
2022-08-26 18:54:17 +0430

    Remove email filed, add username field to user class and change import NotNull and Size annotations

--- src/main/java/io/github/mojilinux/nabApp/model/User.java
+++ src/main/java/io/github/mojilinux/nabApp/model/User.java
@@ -1,27 +1,20 @@
 package io.github.mojilinux.nabApp.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "nab_user")
 public class User extends BaseEntity<Long>{
-
-    @JsonIgnore
-    @NotNull
-    @Size(min = 60, max = 60)
-    @Column(name = "password_hash")
-    private String password;
 
     @Size(max = 50)
     @Column(name = "first_name")
@@ -31,9 +24,16 @@ public class User extends BaseEntity<Long>{
     @Column(name = "last_name")
     private String lastName;
 
-    @Email
+    @Column(name = "user_name")
+    @NotNull
     @Size(min = 5, max = 254)
-    private String email;
+    private String username;
+
+    @JsonIgnore
+    @NotNull
+    @Size(min = 60, max = 60)
+    @Column(name = "password_hash")
+    private String password;
 
     @NotNull
     private boolean activated = false;
