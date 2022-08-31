e06e7ecc257f0626ecd36955820cb57a5ad3ed8c
mojilinux <mojilinux@gmail.com>
2022-08-31 22:29:52 +0430

    Add CrossOrigin annotation to UserController

--- src/main/java/io/github/mojilinux/nabApp/web/controller/UserController.java
+++ src/main/java/io/github/mojilinux/nabApp/web/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path = "/api/users", produces = "application/json")
 public class UserController {
 
