9588d34967d16eb4bd15f46c0254f4f73d5e7eca
iman <93901983+imanabyar@users.noreply.github.com>
2022-10-17 19:47:23 +0330

    test JSP

--- src/main/java/io/github/mojilinux/nabApp/NabAppApplication.java
+++ src/main/java/io/github/mojilinux/nabApp/NabAppApplication.java
@@ -2,9 +2,16 @@ package io.github.mojilinux.nabApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NabAppApplication {
+public class NabAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NabAppApplication.class, args);
